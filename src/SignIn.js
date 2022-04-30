93b55e44f4f71a2b2e48fdea23bf2d21da18d2ca
Wojtek Książek <wojtek99955@gmail.com>
2022-04-30 13:26:01 +0200

    SignIn formik added

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
-import { Formik, Form } from "formik";
+import { Formik, Form, Field } from "formik";
 import { LockOutline } from "@styled-icons/evaicons-outline/LockOutline";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const Container = styled.div`
   display: flex;
@@ -20,6 +22,39 @@ function SignIn() {
     <Container>
       <LockIcon />
       <h1>Sign In</h1>
+      <Formik
+        initialValues={{
+          username: "",
+          password: "",
+        }}
+        onSubmit={(values) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+          }, 1000);
+        }}
+      >
+        <Form>
+          <Field
+            as={TextField}
+            fullWidth
+            variant="standard"
+            type="text"
+            name="username"
+            placeholder="Username *"
+          />
+          <Field
+            as={TextField}
+            fullWidth
+            variant="standard"
+            placeholder="Password *"
+            type="password"
+            name="password"
+          />
+          <Button variant="contained" fullWidth type="submit">
+            Log in
+          </Button>
+        </Form>
+      </Formik>
     </Container>
   );
 }
