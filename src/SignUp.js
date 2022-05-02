243875ef59e87592d79cb8b7515dad18451778ba
Wojtek Książek <wojtek99955@gmail.com>
2022-05-02 11:40:02 +0200

    SignUp Yup added

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,14 +1,15 @@
 import styled from "styled-components";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import Link from "@mui/material/Link";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 import Checkbox from "@mui/material/Checkbox";
+import * as Yup from "yup";
+import Error from "./Error";
 
 const Container = styled.div`
   display: flex;
@@ -35,12 +36,27 @@ const onSubmit = (values) => {
     alert(JSON.stringify(values, null, 2));
   }, 1000);
 };
+const validationSchema = Yup.object({
+  name: Yup.string().required(),
+  surname: Yup.string().required(),
+  email: Yup.string().email().required(),
+  tel: Yup.number().required(),
+  password: Yup.string().required().min(6),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password")], "Password must match")
+    .required(),
+  termsAndConditions: Yup.string().oneOf(["true"], "Accept terms & conditions"),
+});
 
 function SignUp() {
   return (
     <Container>
       <h1>Sign Up</h1>
-      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
         <Form>
           <Field
             as={TextField}
@@ -49,6 +65,7 @@ function SignUp() {
             variant="standard"
             label="Name *"
           />
+          <ErrorMessage name="name" component={Error} />
           <Field
             as={TextField}
             name="surname"
@@ -56,6 +73,7 @@ function SignUp() {
             variant="standard"
             label="Surname *"
           />
+          <ErrorMessage name="surname" component={Error} />
           <Field
             as={TextField}
             name="email"
@@ -63,6 +81,7 @@ function SignUp() {
             variant="standard"
             label="Email *"
           />
+          <ErrorMessage name="email" component={Error} />
           <FormControl>
             <FormLabel>Gender</FormLabel>
             <RadioGroup name="gender" row>
@@ -74,6 +93,7 @@ function SignUp() {
               <FormControlLabel value="male" control={<Radio />} label="Male" />
             </RadioGroup>
           </FormControl>
+          <ErrorMessage name="gender" component={Error} />
           <Field
             as={TextField}
             name="tel"
@@ -81,6 +101,7 @@ function SignUp() {
             variant="standard"
             label="Phone Number *"
           />
+          <ErrorMessage name="tel" component={Error} />
           <Field
             as={TextField}
             name="password"
@@ -88,6 +109,7 @@ function SignUp() {
             variant="standard"
             label="Password *"
           />
+          <ErrorMessage name="password" component={Error} />
           <Field
             as={TextField}
             name="confirmPassword"
@@ -95,10 +117,12 @@ function SignUp() {
             variant="standard"
             label="ConfirmPassword *"
           />
+          <ErrorMessage name="confirmPassword" component={Error} />
           <FormControlLabel
             control={<Field as={Checkbox} name="termsAndConditions" />}
             label="I agree with Terms and Privacy."
           />
+          <ErrorMessage name="termsAndConditions" component={Error} />
           <Button variant="contained" type="submit">
             Sign Up
           </Button>
