af08435fc85d4acc0c7f0b207ca99b3614a3479c
Wojtek Książek <wojtek99955@gmail.com>
2022-05-02 11:08:31 +0200

    SignIn Yup update

--- src/SignIn.js
+++ src/SignIn.js
@@ -42,44 +42,42 @@ function SignIn({ setValue }) {
         onSubmit={onSubmit}
         validationSchema={schemaValidation}
       >
-        {({ errors, touched }) => (
-          <Form>
-            <Field
-              as={TextField}
-              fullWidth
-              variant="standard"
-              type="text"
-              name="username"
-              label="Username *"
-            />
-            <ErrorMessage name="username" component={Error} />
-            <Field
-              as={TextField}
-              fullWidth
-              variant="standard"
-              label="Password *"
-              type="password"
-              name="password"
-            />
-            <ErrorMessage name="password" component={Error} />
-            <Button
-              sx={{ margin: "1.5rem 0" }}
-              variant="contained"
-              fullWidth
-              type="submit"
-            >
-              Log in
-            </Button>
-            <Link
-              href="#"
-              underline="none"
-              sx={{ fontSize: "0.8rem" }}
-              onClick={handleChangeTab}
-            >
-              Don't have an account? Sign Up here!
-            </Link>
-          </Form>
-        )}
+        <Form>
+          <Field
+            as={TextField}
+            fullWidth
+            variant="standard"
+            type="text"
+            name="username"
+            label="Username *"
+          />
+          <ErrorMessage name="username" component={Error} />
+          <Field
+            as={TextField}
+            fullWidth
+            variant="standard"
+            label="Password *"
+            type="password"
+            name="password"
+          />
+          <ErrorMessage name="password" component={Error} />
+          <Button
+            sx={{ margin: "1.5rem 0" }}
+            variant="contained"
+            fullWidth
+            type="submit"
+          >
+            Log in
+          </Button>
+          <Link
+            href="#"
+            underline="none"
+            sx={{ fontSize: "0.8rem" }}
+            onClick={handleChangeTab}
+          >
+            Don't have an account? Sign Up here!
+          </Link>
+        </Form>
       </Formik>
     </Container>
   );
